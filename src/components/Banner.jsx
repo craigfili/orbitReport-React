5567593ff2551edb5d3b8688127a236f4def32f7
craigfili <craig.filipowicz@gmail.com>
2024-08-28 23:41:03 -0500

    Assignment Complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+import "./styling.css";
+
+export default function Banner () {
+    return (
+    <>
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellitles in that orbit type</p>
+    </>
+    )
+}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,31 @@
+import "./styling.css";
+
 const Table = ({ sat }) => {
+  
+  let satJSX = sat.map((sat) => {
+    let status = sat.operational ? "active" : "inactive";
+    return(
+      <tr id={sat.id}>
+        <td>{sat.name}</td>
+        <td>{sat.type}</td>
+        <td>{sat.launchDate}</td>
+        <td>{status}</td>
+      </tr>
+    )
+  });
   return (
-    <table>
+  <table>
     <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+      <tr>
+        <th>Name</th>
+        <th>Type of Satelite</th>
+        <th>Launch Date</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+      <tbody>
+        {satJSX}
+      </tbody>
    </table>
   );
 };
